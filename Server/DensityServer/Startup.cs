d0ea757b97e65faadf6e099642a6ee2cc0e835de
Danny Dowling <danny.dowling@gmail.com>
2021-05-10 00:17:17 -0800

    nearly working

--- Server/DensityServer/Startup.cs
+++ Server/DensityServer/Startup.cs
@@ -17,29 +17,37 @@ namespace DensityServer
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IWebHostEnvironment environment, IConfiguration configuration)
         {
+            Environment = environment;
             Configuration = configuration;
         }
 
+        public IWebHostEnvironment Environment { get; }
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAntiforgery();
+
+            services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<IdentityUser>>();
+
             services.AddDbContext<AppDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
 
-            services.AddIdentityCore<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddEntityFrameworkStores<AppDbContext>();
+            services.AddIdentity<IdentityUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = true)
+               .AddEntityFrameworkStores<AppDbContext>();
 
+            //the error parameter cannot be null is from the version of .Net.Authorization being 5.0  ... 5/10/21 
             services.AddRazorPages();
-            services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<IdentityUser>>();
 
             services.AddScoped<ICountryRepository, CountryRepository>();            
-            services.AddScoped<IUserRepository, UserRepository>();            
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<ILocationRepository, LocationRepository>();
+            services.AddScoped<IEmployeeRepository, EmployeeRepository>();
             services.AddScoped<MessageModel>();
 
             services.AddHttpClient<ICountryDataService, CountryDataService>(client =>
@@ -49,6 +57,14 @@ namespace DensityServer
             services.AddHttpClient<IUserDataService, UserDataService>(client =>
             { client.BaseAddress = new Uri("https://localhost:44336/");  })
                 .AddHttpMessageHandler(handler => new RetryPolicy(2, TimeSpan.FromSeconds(20)));
+
+            services.AddHttpClient<ILocationDataService, LocationDataService>(client =>
+            { client.BaseAddress = new Uri("https://localhost:44336/"); })
+              .AddHttpMessageHandler(handler => new RetryPolicy(2, TimeSpan.FromSeconds(20)));
+
+            services.AddHttpClient<IEmployeeDataService, EmployeeDataService>(client =>
+            { client.BaseAddress = new Uri("https://localhost:44336/"); })
+              .AddHttpMessageHandler(handler => new RetryPolicy(2, TimeSpan.FromSeconds(20)));
 
             services.AddHttpClient<MessageModel>(client =>
             { client.BaseAddress = new Uri("https://localhost:44336/");  })
@@ -83,7 +99,6 @@ namespace DensityServer
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
-                endpoints.MapBlazorHub();
                 endpoints.MapHub<ChatHub>("/Chat");
                 endpoints.MapFallbackToPage("/_Host");
                
