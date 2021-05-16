e10333282843c8b16c15508197b3a28f91f4555b
Danny Dowling <danny.dowling@gmail.com>
2021-05-16 01:28:50 -0800

    need to start migrations and get databases going...

---
+++ Density/ClientSide/PreFlightChat.js
@@ -0,0 +1,35 @@
+﻿(function () {
+    var preFlightHub = $.connection.preFlightChatHub;
+    $.connection.hub.logging = true;
+    $.connection.hub.start();
+
+    preFlightHub.client.newMessage = function (message) {
+        model.addMessage(message);
+    };
+
+    var Model = function () {
+        var self = this;
+        self.message = ko.observable(""),
+            self.messages = ko.observableArray();
+    };
+
+    Model.prototype = {
+
+        sendMessage: function () {
+            var self = this;
+            preFlightHub.server.send(self.message());
+            self.message("");
+        },
+
+        addMessage: function (message) {
+            var self = this;
+            self.messages.push(message);
+        }
+    };
+
+    var model = new Model();
+
+    $(function () {
+        ko.applyBindings(model);
+    });
+}());
---
+++ Server/DensityServer/Services/Chat/DensityServerChatHub.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNetCore.SignalR;
+
+namespace DensityServer.Hubs
+{
+    public class DensityServerChatHub : Hub
+    {
+        public void Send(string message, string associates)
+        {
+            Clients.Group(associates).SendAsync
+                (
+                Context.User.Identity.Name + ": " + message
+                );
+        }
+    }
+}
